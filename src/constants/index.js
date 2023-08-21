ae8171af2a479393e4c8cc31a591cc974c9e5630
wissem <saadliwissem88@gmail.com>
2023-08-21 14:34:25 +0100

    last commit with buffer error

---
+++ src/constants/index.js
@@ -0,0 +1,276 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  docker,
+  threejs,
+  ES,
+  tak,
+  express,
+  tadeco,
+  eng,
+  mysql,
+  biomarket,
+  pharma,
+  sabri,
+  
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Native Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Content Creator",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+  {
+    name: "mysql",
+    icon: mysql,
+  },
+];
+
+const experiences = [
+  {
+    title: "Full Stack Developer",
+    company_name: "L'école international Française de Gafsa",
+    icon: eng,
+    iconBg: "#383E56",
+    date: "fev 2023 - juin 2023",
+    points: [
+      "Creation of a web platform that connects product buyers direct organic farming with producers with MERN stack.",
+      "Creation of a traceability and tracking system.",
+    ],
+  },
+  {
+    title: "Full Stack Developer",
+    company_name: "Tadeco Group",
+    icon: tadeco,
+    iconBg: "#383E56",
+    date: "March 2020 - April 2020",
+    points: [
+      "Development of a booking site in a hotel with MERN Stack.",
+      "Creation of a Rest API.",
+      "connect to mongoDb database using mongoose.",
+    ],
+  },
+  {
+    title: "React Native Developer",
+    company_name: "ExpressDisplay",
+    icon: express,
+    iconBg: "#E6DEDD",
+    date: "Juil 2022 - Aug 2022",
+    points: [
+      "Front-end of a mobile ticket booking application with React Native.",
+      "Système d'authentification.",
+      "Connexion avec une API Rest.",
+      "Développer des fonctionnalités en construisant un code de qualité:maintenable et évolutif.",
+    ],
+  },
+  {
+    title: "Web Developer",
+    company_name: "Takyour’s Connect",
+    icon: tak,
+    iconBg: "#383E56",
+    date: "Jan 2021 - mars 2021",
+    points: [
+      "• Development of a management system using Java EE.",
+      "Help customers prioritize product features to bring the best value to their business.",
+      " Participate in product planning with client and designer",
+    ],
+  },
+  {
+    title: "JQuery & CSS Developer",
+    company_name: "Development Engineering Services",
+    icon: ES,
+    iconBg: "#E6DEDD",
+    date: "Juin 2020",
+    points: [
+      "Developing a frontend website for an e-library using jQuery, CSS...",
+      "Collaborating with cross-functional teams including designers and others developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I want to appreciate you for the great work ,it was truly an amazing project and i consider it as good progress for my business and keep it up thank you for your service👏👏.",
+    name: "Mohamed Harketi",
+    designation: "IT",
+    company: "Student",
+    image: "https://scontent.fnbe1-2.fna.fbcdn.net/v/t39.30808-1/347242191_642318457371185_7096676794833445748_n.jpg?stp=dst-jpg_p200x200&_nc_cat=100&ccb=1-7&_nc_sid=7206a8&_nc_ohc=ARP7dPFCryMAX-QrgO8&_nc_ht=scontent.fnbe1-2.fna&oh=00_AfCXmzv1rcNGIw3yDHm7oQ560TD8fBz4sU0tyemz7ZFtFQ&oe=64E58F58",
+  },
+  {
+    testimonial:
+      "I wanted to take a moment to express my heartfelt gratitude to the most incredible friend. Thank you from heart for your unwavering commitment to excellence .",
+    name: "Sabri Slougui",
+    designation: "Designer",
+    company: "and Content Creator",
+    image: "https://scontent.fnbe1-2.fna.fbcdn.net/v/t39.30808-1/277158185_2986848604959262_3325121755172944116_n.jpg?stp=c0.8.200.200a_dst-jpg_p200x200&_nc_cat=107&ccb=1-7&_nc_sid=7206a8&_nc_ohc=4RSGr8Sn3LQAX-e1wi4&_nc_ht=scontent.fnbe1-2.fna&oh=00_AfA3mj7NXmQrHmuk0clU87dz60j0h1OZ5-Q7kIPW1C6PDA&oe=64E600F4",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Gaith Nasri",
+    designation: "Nurse",
+    company: "at Sokra clinical",
+    image: "https://scontent.fnbe1-2.fna.fbcdn.net/v/t39.30808-6/331523127_9824275357586362_3999806579662416937_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=zHlVDh1XcwUAX-Rh1Fg&_nc_ht=scontent.fnbe1-2.fna&oh=00_AfCoT2pH-ZfWjmI7VsraT-d7bGZi0GVRRX2ltnI1Uqm9uQ&oe=64E59AE1",
+  },
+];
+
+const projects = [
+  {
+    name: "Bio Market",
+    description:
+      "Bridging Agriculture and Technology for Sustainable Commerce 🌱🚀Empower small farmers and connect conscientious buyers with our innovative MERN-based platform. We're revolutionizing the agricultural landscape by enabling direct interactions between farmers and buyers. Our robust tracking system ensures transparency from seed to table, while cutting-edge security safeguards your transactions. Collaborative capabilities enable small farmers to achieve big orders, fostering growth and prosperity. Join us in fostering a community that values quality, sustainability, and ethical practices. Together, we're redefining how we shop and support local agriculture. #FarmToTableTech",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: biomarket,
+    source_code_link: "https://github.com/saadliwissem",
+  },
+  {
+    name: "MediDrop: Your Health, Delivered. 🏥💊",
+    description:
+      "Experience healthcare at your doorstep with the power of our React Native-based app. Say goodbye to long waits and tedious trips – MediDrop brings your prescribed medicines to you, swiftly and securely. Our user-friendly interface makes ordering a breeze, while real-time updates keep you informed about your delivery. Rest easy knowing that our platform prioritizes your health and safety, with every transaction encrypted and secure. Download the app today and take control of your well-being with convenience, accuracy, and care. #HealthcareInYourHands.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: pharma,
+    source_code_link: "https://github.com/saadliwissem",
+  },
+  {
+    name: "DesignScape: Where Pixels and Words Unite 🎨🖋️",
+    description:
+      "Welcome to Sabri's portfolio, a fusion of design and content crafted using React.js and Tailwind CSS. Immerse yourself in a gallery of captivating visuals, from sleek UI designs to evocative illustrations. Beyond aesthetics, delve into thought-provoking articles that reflect [Friend's Name]'s mastery of storytelling. This portfolio is a testament to their ability to blend technology, creativity, and innovation seamlessly. Join us on this journey of digital artistry and impactful narratives. 🌟📐✍️.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: sabri,
+    source_code_link: "https://github.com/saadliwissem/react-tailwind-Portfolio",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
